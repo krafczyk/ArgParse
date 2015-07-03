9a624b26b1894dc7b93fba59202b4332d88eb2ed
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2015-07-03 14:08:09 -0500

    Add another test to test_2.

--- unittests/test_2.cpp
+++ unittests/test_2.cpp
@@ -23,26 +23,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ArgParse/ArgParse.h"
 
 int main(int argc, char** argv) {
-	int a;
+	int a = 0;
 	std::string output_dir = "";
-	long r;
-	double s;
+	long r = 0;
+	double s = 0;
 
 	bool t = false;
 	bool u = false;
 	bool v = false;
 
+	std::vector<std::string> strings;
+	std::vector<int> ints;
+
+	//ArgParse::DebugLevel = 10;
+
 	ArgParse::ArgParser Parser("This parser demonstrates how to use ArgParse groups.");
 	ArgParse::ArgGroup& NormalGroup = Parser.AddArgGroup("norm", "This is a normal group");
 	NormalGroup.AddArgument("-a", "Test argument for a normal group", &a);
-	NormalGroup.AddArgument("-o/--output-dir", "Test required argument", &output_dir, ArgParse::Argument::Required);
+	NormalGroup.AddArgument("-o/--output-dir", "Test required argument", &output_dir, ArgParse::ArgObject::Required);
 	ArgParse::ArgGroup& RequiredInclusiveGroup = Parser.AddInclusiveArgGroup("required-inclusive", "This is a required Inclusive group", ArgParse::ArgObject::None, ArgParse::ArgObject::Required);
-	RequiredInclusiveGroup.AddArgument("-r", "Test argument", &r, ArgParse::Argument::Required);
-	RequiredInclusiveGroup.AddArgument("-s", "Test argument", &s, ArgParse::Argument::Required);
+	RequiredInclusiveGroup.AddArgument("-r", "Test argument", &r, ArgParse::ArgObject::Required);
+	RequiredInclusiveGroup.AddArgument("-s", "Test argument", &s, ArgParse::ArgObject::Required);
 	ArgParse::ArgGroup& RequiredExclusiveGroup = Parser.AddExclusiveArgGroup("required-exclusive", "This is a required Exclusive group", ArgParse::ArgObject::None, ArgParse::ArgObject::Required);
-	RequiredExclusiveGroup.AddArgument("-t", "Test argument", &t, ArgParse::Argument::Required);
-	RequiredExclusiveGroup.AddArgument("-u", "Test argument", &u, ArgParse::Argument::Required);
-	RequiredExclusiveGroup.AddArgument("-v", "Test argument", &v, ArgParse::Argument::Required);
+	RequiredExclusiveGroup.AddArgument("-t", "Test argument", &t, ArgParse::ArgObject::Required);
+	RequiredExclusiveGroup.AddArgument("-u", "Test argument", &u, ArgParse::ArgObject::Required);
+	RequiredExclusiveGroup.AddArgument("-v", "Test argument", &v, ArgParse::ArgObject::Required);
+	ArgParse::ArgGroup& InclusiveMultipleGroup = Parser.AddInclusiveArgGroup("multiple-inclusive", "This is a multiple inclusive group", ArgParse::ArgObject::Multiple);
+	InclusiveMultipleGroup.AddArgument("-i", "Test argument", &ints);
+	InclusiveMultipleGroup.AddArgument("-str", "Test argument", &strings);
 	
 	if(Parser.ParseArgs(argc, argv) < 0) {
 		fprintf(stderr, "There was a problem parsing the arguments!\n");
@@ -67,6 +75,20 @@ int main(int argc, char** argv) {
 
 	if(v) {
 		printf("v was defined!\n");
+	}
+
+	if(ints.size() != 0) {
+		printf("ints:\n");
+		for(size_t i=0; i<ints.size(); ++i) {
+			printf("%i\n", ints[i]);
+		}
+	}
+
+	if(strings.size() != 0) {
+		printf("strings:\n");
+		for(size_t i=0; i<strings.size(); ++i) {
+			printf("%s\n", strings[i].c_str());
+		}
 	}
 
 	if(argc > 1) {
